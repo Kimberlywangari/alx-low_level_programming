2c89343f300d9dc617f7696b9094b41653b1ed11
kimberlywangari <kimberlywangarike@gmail.com>
2023-06-15 16:29:51 -0700

    yut

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - program a printing line using print functionf
- * return: 0 (success)
+ * main - program a printing line using print function with return value 0
+ * return is integer: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
