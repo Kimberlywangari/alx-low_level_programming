fe20bf8f244bd70eb80b349420ee50c4d7b0674f
kimberlywangari <kimberlywangarike@gmail.com>
2023-06-15 15:51:16 -0700

    wiw

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - orint using puts not printf
+ *return is an integer
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
