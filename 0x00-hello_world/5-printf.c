daa6a308adb379b1d388004045ca8ffcc976bb9f
kimberlywangari <kimberlywangarike@gmail.com>
2023-06-15 15:59:29 -0700

    wow

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -program printing line using printf
+ * return: 0(correct)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
